93fd977bf06291970eee62bc3787a15f0cfd7c5f
ninaklaus <906582199@qq.com>
2025-10-18 10:48:18 +0800

    init: 项目初始化

---
+++ src/main/java/com/home/ms4j/LcBackendApplication.java
@@ -0,0 +1,11 @@
+package com.home.ms4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LcBackendApplication.class, args);
+    }
+} 
